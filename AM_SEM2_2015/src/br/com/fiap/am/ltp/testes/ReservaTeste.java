50c2be645c934f814103b3eb1496c79b3d9d553e
Lucas Gomes <lucasluismgomes@gmail.com>
2015-10-21 21:37:07 -0200

    ReservaTeste e Banco atualizados.

---
+++ AM_SEM2_2015/src/br/com/fiap/am/ltp/testes/ReservaTeste.java
@@ -0,0 +1,126 @@
+package br.com.fiap.am.ltp.testes;
+
+import java.sql.Connection;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+
+import javax.swing.JOptionPane;
+
+
+
+
+
+
+
+
+
+
+
+
+import br.com.fiap.am.ltp.beans.Cliente;
+import br.com.fiap.am.ltp.beans.Funcionario;
+import br.com.fiap.am.ltp.beans.Quarto;
+import br.com.fiap.am.ltp.beans.Reserva;
+import br.com.fiap.am.ltp.beans.TipoQuarto;
+import br.com.fiap.am.ltp.bo.ReservaBO;
+import br.com.fiap.am.ltp.conexao.ConexaoFactory;
+import br.com.fiap.am.ltp.excecoes.Excecao;
+
+public class ReservaTeste {
+
+	public static void main(String[] args) throws Excecao {
+		Connection conexao = null;
+		try {
+			int funcionalidade = Integer.parseInt(JOptionPane.showInputDialog("Qual funcionalidade deseja testar?\n\n"
+					+ "1 - Calcular Valor da Reserva\n" + "2 - Editar\n" + "3 - Buscar Todos\n" + "4 - Apagar\n" + "5 - Buscar por ID\n"));
+
+			if (funcionalidade == 1) {
+				conexao = ConexaoFactory.controlarInstancia().getConnection("OPS$74795", "251295");
+				
+				Reserva reserva = new Reserva();
+				Cliente cliente = new Cliente();
+				Funcionario funcionario = new Funcionario();
+				List<Quarto> lstQuarto = new ArrayList<Quarto>();
+				double valorReserva = 0;
+
+				Calendar c = Calendar.getInstance();
+
+				String data = JOptionPane
+						.showInputDialog("Qual a data de ENTRADA do cliente? Formato: DD/MM/AAAA");
+
+				while (data.length() < 10) {
+					data = JOptionPane.showInputDialog("DATA ERRADA! Use o Formato: DD/MM/AAAA ex: 25/12/1995)");
+				}
+
+				int dia = Integer.parseInt(data.substring(0, 2));
+				int mes = Integer.parseInt(data.substring(3, 5));
+				int ano = Integer.parseInt(data.substring(6, 10));
+
+				c.set(ano, (mes - 1), dia);
+				
+				Calendar c2 = Calendar.getInstance();
+
+				String data2 = JOptionPane
+						.showInputDialog("Qual a data de SAÍDA do cliente? Formato: DD/MM/AAAA");
+
+				while (data2.length() < 10) {
+					data2 = JOptionPane.showInputDialog("DATA ERRADA! Use o Formato: DD/MM/AAAA ex: 25/12/1995)");
+				}
+
+				int dia2 = Integer.parseInt(data.substring(0, 2));
+				int mes2 = Integer.parseInt(data.substring(3, 5));
+				int ano2 = Integer.parseInt(data.substring(6, 10));
+
+				c.set(ano2, (mes2 - 1), dia2);
+				
+				reserva.setCliente(cliente);
+				reserva.setFuncionario(funcionario);
+				reserva.setDtEntrada(c);
+				reserva.setDtSaida(c2);
+				
+				Quarto quarto1 = new Quarto();
+				TipoQuarto tipoQuarto1 = new TipoQuarto();
+				
+				tipoQuarto1.setCodigo(Integer.parseInt(JOptionPane.showInputDialog("Qual o código do tipo do quarto?")));
+				
+				quarto1.setTipo(tipoQuarto1);
+				
+				
+				Quarto quarto2 = new Quarto();
+					TipoQuarto tipoQuarto1 = new TipoQuarto();
+				
+				tipoQuarto1.setCodigo(Integer.parseInt(JOptionPane.showInputDialog("Qual o código do tipo do quarto?")));
+				
+				quarto1.setTipo(tipoQuarto1);
+				
+				reserva.setQuarto(lstQuarto);					
+				
+				new ReservaBO();
+				valorReserva = ReservaBO.calcularReserva(reserva, conexao);
+				
+				System.out.println("O valor da reserva ficou: R$ " + valorReserva);
+				
+			} else if (funcionalidade == 2) {
+				// Código de edição
+			} else if (funcionalidade == 3) {
+				// Código de consulta
+			} else if (funcionalidade == 4) {
+				// Código de deletar
+			} else if (funcionalidade == 5) {
+				// Código de buscar por código
+			} else {
+				JOptionPane.showMessageDialog(null, "Essa funcionalidade não existe! Tente novamente");
+			}
+		} catch (Exception e) {
+			throw new Excecao(e);
+		} finally {
+			try {
+				conexao.close();
+			} catch (Exception e) {
+				throw new Excecao(e);
+			}
+		}
+	}
+
+}
