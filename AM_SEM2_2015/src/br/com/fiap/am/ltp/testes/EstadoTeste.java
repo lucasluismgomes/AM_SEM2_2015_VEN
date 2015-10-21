85792d7684534229496775262becddf091cd8f41
Mateus de Souza <mateus_silva1503@hotmail.com>
2015-10-21 21:36:13 -0200

    Classes do estado criados e metodos tbm

---
+++ AM_SEM2_2015/src/br/com/fiap/am/ltp/testes/EstadoTeste.java
@@ -0,0 +1,133 @@
+package br.com.fiap.am.ltp.testes;
+
+import java.sql.Connection;
+import java.util.List;
+
+import javax.swing.JOptionPane;
+
+import br.com.fiap.am.ltp.beans.Estado;
+import br.com.fiap.am.ltp.bo.EstadoBO;
+import br.com.fiap.am.ltp.conexao.ConexaoFactory;
+import br.com.fiap.am.ltp.excecoes.Excecao;
+
+public class EstadoTeste 
+{
+	/**
+	 * Classe para teste dos metodos criados na classes CidadeDAO e CidadeBO.
+	 * @author Mateus rm74793
+	 * @throws Excecao
+	 * @since 1.0
+	 * @see Cidade,CidadeBO,Estado
+	 */
+	public static void main(String[] args) throws Excecao 
+	{
+		Connection conexao =  null;
+		try 
+		{
+			int funcionalidade = Integer.parseInt(JOptionPane
+					.showInputDialog("Qual funcionalidade deseja testar?\n\n"
+							+ "1 - Gravar\n" + "2 - Editar\n"
+							+ "3 - Buscar Todos\n" + "4 - Apagar\n"
+							+ "5 - Buscar por ID\n"));
+			conexao = ConexaoFactory.controlarInstancia()
+					.getConnection("OPS$RM74793", "150395");
+			
+			if (funcionalidade == 1) 
+			{
+				Estado estado = new Estado();
+				EstadoBO estadoBO = new EstadoBO();
+				estado.setNome(JOptionPane.showInputDialog("Informe o nome do estado"));
+				estado.setSigla(JOptionPane.showInputDialog("Informe a sigla do estado"));
+				boolean resultado = estadoBO.gravar(estado, conexao);
+				if(!resultado)
+				{
+					System.out.println("Gravou!");
+				}
+				else
+				{
+					System.out.println("Não Gravou!");
+				}
+			}
+			else if (funcionalidade == 2) 
+			{
+				Estado estado = new Estado();
+				EstadoBO estadoBO = new EstadoBO();
+				List<Estado> lstEstado = estadoBO.buscarTodos(conexao);
+				for (Estado estado2 : lstEstado) 
+				{
+					System.out.println("Código: "+estado2.getCodigo()+" Nome: "+estado2.getNome()+" Sigla: "+estado2.getSigla());
+				}
+				estado.setCodigo(Integer.parseInt(JOptionPane.showInputDialog("Informe o codigo a ser editado")));
+				estado.setNome(JOptionPane.showInputDialog("Informe o nome do estado"));
+				estado.setSigla(JOptionPane.showInputDialog("Informe a sigla do estado"));
+				boolean resultado = estadoBO.gravar(estado, conexao);
+				if(!resultado)
+				{
+					System.out.println("Atualizou!");
+				}
+				else
+				{
+					System.out.println("Não Atualizou!");
+				}
+			}
+			else if (funcionalidade == 3) 
+			{
+				EstadoBO estadoBO = new EstadoBO();
+				List<Estado> lstEstado = estadoBO.buscarTodos(conexao);
+				for (Estado estado : lstEstado) 
+				{
+					System.out.println("Código: "+estado.getCodigo()+" Nome: "+estado.getNome()+" Sigla: "+estado.getSigla());
+				}
+			}
+			else if (funcionalidade == 4) 
+			{
+				Estado estado = new Estado();
+				EstadoBO estadoBO = new EstadoBO();
+				List<Estado> lstEstado = estadoBO.buscarTodos(conexao);
+				for (Estado estado2 : lstEstado) 
+				{
+					System.out.println("Código: "+estado2.getCodigo()+" Nome: "+estado2.getNome()+" Sigla: "+estado2.getSigla());
+				}
+				estado.setCodigo(Integer.parseInt(JOptionPane.showInputDialog("Informe o codigo a ser deletado")));
+				boolean resultado = estadoBO.excluir(estado, conexao);
+				if(!resultado)
+				{
+					System.out.println("Apagou!");
+				}
+				else
+				{
+					System.out.println("Não Apagou");
+				}
+				System.out.println("la");
+			}
+			else if (funcionalidade == 5) 
+			{
+				Estado estado = new Estado();
+				EstadoBO estadoBO = new EstadoBO();
+				estado.setNome(JOptionPane.showInputDialog("Nome a ser pesquisado"));
+				List<Estado> lstEstado = estadoBO.buscarPorNome(estado, conexao);
+				for (Estado estado2 : lstEstado) 
+				{
+					System.out.println("Código: "+estado2.getCodigo()+" Nome: "+estado2.getNome()+" Sigla: "+estado2.getSigla());
+				}
+			} 
+			else 
+			{
+				JOptionPane.showMessageDialog(null,
+						"Essa funcionalidade não existe! Tente novamente");
+			}
+			
+		}
+		catch (Exception e) 
+		{
+			throw new Excecao(e);
+		} finally {
+			try {
+				conexao.close();
+			} catch (Exception e) {
+				throw new Excecao(e);
+			}
+		}
+	}
+}
+
